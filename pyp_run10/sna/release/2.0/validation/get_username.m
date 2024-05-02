540e3a98482990fd6f055fb3e3c8c2e3abf9d2a1
liangxuntan <104157691+liangxuntan@users.noreply.github.com>
2024-05-01 18:12:47 -0700

    Delete pyp_run10 directory

--- pyp_run10/sna/release/2.0/validation/get_username.m
+++
@@ -1,8 +0,0 @@
-function userID=get_username()
-  SnA_user_identity_file = '.SnA_user_identity';
-  system(['whoami > ' SnA_user_identity_file]);
-  fid = fopen(SnA_user_identity_file,'r');
-  userID = fscanf(fid,'%s');
-  fclose(fid);
-  delete(SnA_user_identity_file);
-% function get_username
